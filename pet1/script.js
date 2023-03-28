77c39bd1ccbe854130f2dad5f20886cbe8a46b2e
DenisLK <Denul73@gmail.com>
2023-03-28 19:53:35 +0300

    ok

---
+++ pet1/script.js
@@ -0,0 +1,46 @@
+document.querySelector('.contentChung-prev').addEventListener('click', function(){
+   
+    
+    moveClass('port-item-active', 'previousElementSibling');
+
+    const elem = moveClass('time-line-active', 'previousElementSibling');
+    if(elem){
+        elem.querySelector('.time-line-inner').style.width = '';
+    }
+});
+
+
+document.querySelector('.contentChung-next').addEventListener('click', next);
+
+function next(){
+   const elem =  moveClass('time-line-active', 'nextElementSibling');
+    moveClass('port-item-active', 'nextElementSibling');
+    if(elem){
+        elem.querySelector('.time-line-inner').style.width = '';
+    }
+}
+
+function moveClass(className, method){
+    const active = document.querySelector('.' + className);
+    const next = active[method];
+
+    if(next){
+        active.classList.remove(className);
+        next.classList.add(className);
+        return active;
+    }
+    return null;
+};
+const step = 1;
+
+
+setInterval(() => {
+    const active = document.querySelector('.time-line-active').querySelector('.time-line-inner');
+    let w = parseFloat(active.style.width) || 0;
+    if(w === 100){
+      next();        
+        return;
+    }
+    active.style.width = String(w + step) + '%';
+    
+},10*1000*step/100)
