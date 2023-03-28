c40efa78074736bad7552bd218fdb477cc1e14b2
DenisLK <Denul73@gmail.com>
2023-03-28 12:27:58 +0300

    ok

--- pet2/index.js
+++ pet2/index.js
@@ -1,96 +1,47 @@
-function initPlayer(params) {
-    const target = params.target;
-    if (target == null || params.slides === undefined) {
-        return;
-    }
-    let timeLine = '';
-    let contentChung = '';
-    let isFirst = true;
-    for (const elem of params.slides) {
-        timeLine += `
-    <div class="time-line ${isFirst ? 'time-line-active' : ''}">
-        <div class="time-line-inner"></div>
-    </div>`;
-        contentChung += `
-    <div class="content-chung ${isFirst ? 'content-chung-active' : ''}">
-         <img src="${elem.url}" alt="${elem.alt || ''}">
-    </div>`
-        isFirst = false;
-    }
-    target.innerHTML = `
-    <div class="player">
-        <div class="scroll_line ${timeLine}"></div>
-
-        <div class="content-wrapper">
-        <div class="content-switcher contentChung-prev"></div>
-        <div class="content-switcher contentChung-next"></div>
-        <div class="content">${contentChung}</div>
-            
-        </div>
-    
-    </div>`;
-
-
-
-
+document.querySelector('.contentChung-prev').addEventListener('click', function(){
    
-}
-
-
-let nextElem = document.querySelector('.contentChung-prev');
-nextElem.addEventListener('click', function () {
+    const elem = moveClass('time-line-active', 'previousElementSibling');
     moveClass('content-chung-active', 'previousElementSibling');
-    moveClass('time-line-active', 'previousElementSibling', (elem) => {
-        const inner = elem.querySelector('.time-line-inner')
-        const w = parseFloat(inner.style.width) || 0;
+    if(elem){
         elem.querySelector('.time-line-inner').style.width = '';
-        return w <= 10;
-    });
-
+    }
 });
 
 
 document.querySelector('.contentChung-next').addEventListener('click', next);
 
-
-function moveClass(className, method, pred) {
-    const active = target.querySelector('.' + className);
-    let next = active[method];
-    if (pred && !pred(active)) {
-        return null;
+function next(){
+   const elem =  moveClass('time-line-active', 'nextElementSibling');
+    moveClass('content-chung-active', 'nextElementSibling');
+    if(elem){
+        elem.querySelector('.time-line-inner').style.width = '';
     }
-    if (next) {
+}
+
+function moveClass(className, method){
+    const active = document.querySelector('.' + className);
+    const next = active[method];
+
+    if(next){
         active.classList.remove(className);
         next.classList.add(className);
         return active;
     }
     return null;
-}
-
-function next() {
-
-    moveClass('content-chung-active', 'nextElementSibling');
-    const elem = moveClass('time-line-active', 'nextElementSibling');
-    if (elem) {
-        elem.querySelector('.time-line-inner').style.width = '';
-    }
 };
+const step = 1;
 
-let timer;
-function runInterval(time, step) {
-    clearInterval(timer);
-    timer = setInterval(() => {
-        const active = target.querySelector('.time-line-active').querySelector('.time-line-inner');
-        let w = parseFloat(active.style.width) || 0;
-        if (w === 100) {
-            next()
-            return;
-        }
-        active.style.width = String(w + step) + '%'
 
-    }, time * 1000 * step / 100);
-}
-runInterval((params.time) || 1, (params.step) || 1)
+setInterval(() => {
+    const active = document.querySelector('.time-line-active').querySelector('.time-line-inner');
+    let w = parseFloat(active.style.width) || 0;
+    if(w === 100){
+      next();        
+        return;
+    }
+    active.style.width = String(w + step) + '%';
+    
+},2*1000*step/100)
 
 
 
