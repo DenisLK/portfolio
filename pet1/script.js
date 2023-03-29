df8972d2312d7ca0b59d1253f76a6532913fdb20
DenisLK <Denul73@gmail.com>
2023-03-29 20:27:43 +0300

    ok

--- pet1/script.js
+++ pet1/script.js
@@ -1,10 +1,10 @@
-document.querySelector('.contentChung-prev').addEventListener('click', function(){
-   
-    
+document.querySelector('.contentChung-prev').addEventListener('click', function () {
+
+
     moveClass('port-item-active', 'previousElementSibling');
 
     const elem = moveClass('time-line-active', 'previousElementSibling');
-    if(elem){
+    if (elem) {
         elem.querySelector('.time-line-inner').style.width = '';
     }
 });
@@ -12,19 +12,19 @@ document.querySelector('.contentChung-prev').addEventListener('click', function(
 
 document.querySelector('.contentChung-next').addEventListener('click', next);
 
-function next(){
-   const elem =  moveClass('time-line-active', 'nextElementSibling');
+function next() {
+    const elem = moveClass('time-line-active', 'nextElementSibling');
     moveClass('port-item-active', 'nextElementSibling');
-    if(elem){
+    if (elem) {
         elem.querySelector('.time-line-inner').style.width = '';
     }
 }
 
-function moveClass(className, method){
+function moveClass(className, method) {
     const active = document.querySelector('.' + className);
     const next = active[method];
 
-    if(next){
+    if (next) {
         active.classList.remove(className);
         next.classList.add(className);
         return active;
@@ -37,10 +37,24 @@ const step = 1;
 setInterval(() => {
     const active = document.querySelector('.time-line-active').querySelector('.time-line-inner');
     let w = parseFloat(active.style.width) || 0;
-    if(w === 100){
-      next();        
+    if (w === 100) {
+        next();
         return;
     }
     active.style.width = String(w + step) + '%';
-    
-},10*1000*step/100)
+
+}, 10 * 1000 * step / 100)
+
+
+
+
+
+
+
+document.querySelector('.link_resume').addEventListener('click', function () {
+    if(document.querySelector('.hid-link').classList.contains('hid-link-active')){
+        document.querySelector('.hid-link').classList.remove('hid-link-active');
+    }else{
+    document.querySelector('.hid-link').classList.add('hid-link-active');
+    }
+});
