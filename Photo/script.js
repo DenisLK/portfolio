2a3b12122d56ba404b7efc8ac97ee8bbc87e5d5e
DenisLK <Denul73@gmail.com>
2023-04-05 00:30:53 +0300

    разобрался с api

--- Photo/script.js
+++ Photo/script.js
@@ -1,30 +1,39 @@
 // import { createClient } from 'pexels';
-const client = createClient('tFsLuZZLqOk4rbXhkmN0TjLj8tHqsZ3sQp4AbEB6VBwqCX1aL8Biqoc9');
+// const client = createClient('tFsLuZZLqOk4rbXhkmN0TjLj8tHqsZ3sQp4AbEB6VBwqCX1aL8Biqoc9');
 
-const userID = 'tFsLuZZLqOk4rbXhkmN0TjLj8tHqsZ3sQp4AbEB6VBwqCX1aL8Biqoc9'
+// const userID = 'tFsLuZZLqOk4rbXhkmN0TjLj8tHqsZ3sQp4AbEB6VBwqCX1aL8Biqoc9'
 const photoEl = document.getElementById('photo');
 
 let state = [];
 
 const runPhoto = async () => {
-    const urls = `https://api.pexels.com/v1/?client`;
+    const urls = `https://api.artic.edu/api/v1/artworks?ids=27992,28560&fields=id,title,image_id`;
     const response = await fetch(urls);
     const data = await response.json();
-    state = data;
+    state = data.data;
     setPhoto();
-
+console.log(state);
 };
 
 const render = () =>{
     return state
-    .map(({url,author}) => {
-        return `<div class="photo-wrap" style="background-image: url(${url})">
+    .map(({image_id,title}) => {
+        return `<div class="photo-wrap" style="background-image: url(https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg)" style="width=200px">
         <div class="photo-text">
             <span>Author</span>
-            ${author}
+            ${title}
         </div>
     </div>`
 }).join("");
+//     return state.data(({image_id}) => {
+//         return `<div class="photo-wrap" style="background-image: url(https://www.artic.edu/iiif/2${image_id}/full/843,/0/default.jpg)">
+//         <div class="photo-text">
+//             <span>Author</span>
+            
+//         </div>
+//     </div>`
+// }).join("");
+    
 }
 const setPhoto = () => {
     photoEl.innerHTML = render();
@@ -33,4 +42,3 @@ const setPhoto = () => {
 
 
 runPhoto();
-console.log(state);
