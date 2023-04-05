42d326b69e69d5cc58f91755b8c8100c4f648608
DenisLK <Denul73@gmail.com>
2023-04-05 19:31:56 +0300

    add filter

--- Photo/script.js
+++ Photo/script.js
@@ -3,27 +3,63 @@ const photoEl = document.getElementById('photo');
 let state = [];
 
 const runPhoto = async () => {
-    const urls = `https://api.artic.edu/api/v1/artworks?ids=27992,28560&fields=id,title,image_id`;
+    const urls = `https://api.artic.edu/api/v1/artworks?ids=27992,28560,109620,94100,93759,102739,96973,145301&fields=id,title,image_id`;
     const response = await fetch(urls);
     const data = await response.json();
     state = data.data;
     setPhoto();
 };
 
-const render = () =>{
+const render = () => {
     return state
-    .map(({image_id,title}) => {
-        return `<div class="photo-wrap" style="background-image: url(https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg)" style="width=200px">
+        .map(({ image_id, title }) => {
+            return `<div class="photo-wrap photo-none">
         <div class="photo-text">
-            <span>Author</span>
-            ${title}
+            <span>${title}</span>            
         </div>
+        <img src="https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg" alt="${title}" class="prview">
+        <i class="fa-solid delete fa-circle-xmark"></i>
+        <i class="fa-regular like fa-heart"></i>
     </div>`
-}).join("");
+        }).join("");
 }
 const setPhoto = () => {
     photoEl.innerHTML = render();
 };
+runPhoto();
 
 
+document.addEventListener('click', e => {
+    if (!e.target.classList.contains("delete")) {
+        return;
+    } e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+});
+document.addEventListener('click', e => {
+    if (e.target.classList.contains('fa-regular')) {
+        e.target.classList.add('fa-solid');
+        e.target.classList.remove('fa-regular');
+        // e.target.parentNode.classList.add('likes');
+        e.target.parentNode.classList.remove('photo-none');
+    } else {
+        if (e.target.classList.contains('fa-solid')) {
+            e.target.classList.add('fa-regular');
+            e.target.classList.remove('fa-solid');
+            // e.target.parentNode.classList.remove('likes');
+        }
+    }
+});
+const filterEl = document.getElementById('filter');
+
+filterEl.onclick = function () {
+
+    const noLikeEl = document.querySelectorAll('.photo-none');
+    noLikeEl.forEach(function (item) {
+        if (!item.classList.contains('none')){
+            item.classList.add('none');
+        }else{
+            item.classList.remove('none')
+        }
+});
+
+}
 
